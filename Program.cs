e8000d8c9c3518b6aacbc2ffd317e8df070947d7
Junaid Siddiqui <junaidas97@gmail.com>
2016-01-26 23:08:25 -0500

    Part 2 of idk
    A little nicer to use :)

--- Program.cs
+++ Program.cs
@@ -11,14 +11,11 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             char[,] x = new char[16, 10];
-            bool once = false;
-            while (true)
-            {
-                Random rand = new Random();
+            
+               Random rand = new Random();
 
 
                 //initialized
-                if (!once) {
                     for (int i = 0; i < x.GetLength(0); i++)
                     {
                         for (int j = 0; j < x.GetLength(1); j++)
@@ -28,7 +25,10 @@ namespace ConsoleApplication1
                         }
                         Console.Write("\n");
                     }
-                }
+
+
+            while (true)
+            {
 
                 Console.Write("\n");
                 //time for some fun
@@ -64,7 +64,8 @@ namespace ConsoleApplication1
                     }
                 }
 
-               Console.ReadLine();
+                Console.ReadLine();
+                Console.Clear();
                 x = y;
                 for (int i = 0; i < x.GetLength(0); i++)
                 {
@@ -75,7 +76,7 @@ namespace ConsoleApplication1
                     Console.Write("\n");
                 }
 
-               Console.ReadLine();
+               
             }
         }
         static char AliveOrDead(int count, bool dead_question_mark)
@@ -87,12 +88,12 @@ namespace ConsoleApplication1
             }
             else if (!dead_question_mark)
             {
-                if (count == 3 || count == 2)  return 'O';
+                if (count == 3 || count == 2) return 'O';
                 else return '-';
 
             }
             return '-';
-            
+
         }
     }
 }
